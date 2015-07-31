20dd3f30e1a794d4c9eb22767c24fc012812769e
3alampro <aljareh4all@gmail.com>
2015-07-31 23:35:19 +0300

    changing index view
    updating index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,10 +19,13 @@
             height: 300px;
             overflow-y: scroll;
         }
+        .padding-top {
+            padding-top: 40px;
+        }
     </style>
 </head>
 <body>
-<div class="container">
+<div class="container padding-top">
     <div class="col-md-6">
         <div class="content">
 
@@ -41,17 +44,20 @@
         </div>
     </div>
     <div class="col-md-6">
-        <br>
-        <br>
-        <br>
-        <br>
         <form action="{{ url() }}/" method="POST">
             <span class="loading" style="display: none; font-size: 18px;">
                 sending ...
             </span>
             {{ csrf_field() }}
-            <input type="text" name="name" placeholder="Name:" class="form-control" required>
-            <input type="text" name="message" id="message"  placeholder="Your message:" required class="form-control">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" name="name" placeholder="Name:" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="message">Message:</label>
+                <input type="text" name="message" id="message"  placeholder="Your message:" required class="form-control">
+            </div>
+
             <input type="submit" class="btn btn-primary" value="Send">
 
         </form>
