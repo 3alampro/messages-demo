2e704819a75179993ebb56bbbeb0249b0b1d9877
A.AlGethami <A.AlGethami@tamkeen.it>
2015-07-31 23:56:31 +0300

    design improve

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,6 +19,7 @@
             height: 300px;
             overflow-y: scroll;
         }
+
         .padding-top {
             padding-top: 40px;
         }
@@ -27,47 +28,59 @@
 <body>
 <div class="container padding-top">
     <div class="col-md-6">
-        <div class="content">
 
-            <table class="table">
-                <tr>
-                    <th>From</th>
-                    <th>Message</th>
-                </tr>
-                @foreach($chats as $chat)
-                    <tr>
-                        <td>{{ $chat->name }}</td>
-                        <td>{{ $chat->message }}</td>
-                    </tr>
-                @endforeach
-            </table>
+        <div class="panel panel-default">
+            <div class="panel-heading">Messages</div>
+            <div class="panel-body">
+                <div class="content">
+                    <table class="table">
+                        <tr>
+                            <th>From</th>
+                            <th>Message</th>
+                        </tr>
+                        @foreach($chats as $chat)
+                            <tr>
+                                <td>{{ $chat->name }}</td>
+                                <td>{{ $chat->message }}</td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
     <div class="col-md-6">
-        <form action="{{ url() }}/" method="POST">
-            <span class="loading" style="display: none; font-size: 18px;">
-                sending ...
-            </span>
-            {{ csrf_field() }}
-            <div class="form-group">
-                <label for="name">Name:</label>
-                <input type="text" name="name" placeholder="Name:" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="message">Message:</label>
-                <input type="text" name="message" id="message"  placeholder="Your message:" required class="form-control">
-            </div>
 
-            <input type="submit" class="btn btn-primary" value="Send">
+        <div class="panel panel-default">
+            <div class="panel-heading">New Message</div>
+            <div class="panel-body">
+                <form action="{{ url() }}/" method="POST">
+                    <span class="loading" style="display: none; font-size: 18px;">
+                        sending ...
+                    </span>
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" name="name" placeholder="Name:" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="message">Message:</label>
+                        <input type="text" name="message" id="message" placeholder="Your message:" required
+                               class="form-control">
+                    </div>
 
-        </form>
+                    <input type="submit" class="btn btn-primary" value="Send">
+
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 
 <script src="https://js.pusher.com/2.2/pusher.min.js"></script>
 <script>
     // Enable pusher logging - don't include this in production
-    Pusher.log = function(message) {
+    Pusher.log = function (message) {
         if (window.console && window.console.log) {
             window.console.log(message);
         }
@@ -79,8 +92,8 @@
 
     var channel = pusher.subscribe('new-message');
 
-    channel.bind('App\\Events\\newMessage', function(data) {
-        $('.table').append('<tr><td>'+ data.message.name + '</td><td>'+ data.message.message + '</td></tr>')
+    channel.bind('App\\Events\\newMessage', function (data) {
+        $('.table').append('<tr><td>' + data.message.name + '</td><td>' + data.message.message + '</td></tr>')
     });
 
     $('form').submit(function (e) {
